3c55bc0ce0fe59b9850bf6bc1a2623401c051117
vBoBv <vannponhvath@gmail.com>
2021-02-19 13:57:13 +1300

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,24 @@ import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+						Fib Calculator version K8s!
+					</a>
+					<Link to='/'>Home</Link>
+					<Link to='/otherpage'>Other Page</Link>
+				</header>
+				<div>
+					<Route exact path='/' component={Fib} />
+					<Route path='/otherpage' component={OtherPage} />
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
